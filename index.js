a0af8e26214f6e5c31d50147b045fbbcd7640ce6
Ali Raza Mamdani <52799482+alirazamamdani@users.noreply.github.com>
2022-01-18 15:42:36 +0500

    initial project

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/demo", (req , res) => {
         res.send("Hello")
 } )
-
+app.get("/", (req, res) => {res.send("hello this is main page")})
 app.use("/product" , Product)
 
 app.listen(PORT, () => console.log(`server is running ${PORT}`) )
